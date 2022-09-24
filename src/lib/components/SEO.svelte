66e0215dcb6c39266c97500f331dab431f96a254
Gerardo Castillo <gerrcass@gmail.com>
2022-09-24 10:42:37 -0400

    lib folder structure and SEO component

---
+++ src/lib/components/SEO.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { page } from '$app/stores';
+
+	export let title;
+	export let description;
+	export let language = 'en-us';
+	export let keywords = '';
+	export let type = 'website';
+
+	const url = $page.url.toString();
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta name="keywords" content={keywords} />
+	<meta
+		name="robots"
+		content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+	/>
+	<html lang={language} />
+
+	<!-- https://ogp.me -->
+	<meta property="og:description" content={description} />
+	<meta property="og:title" content={title} />
+	<meta property="og:type" content={type} />
+	<meta property="og:url" content={url} />
+	<!-- <meta property="og:image" content="https://www.example.com/{image}" /> -->
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script>
+	import SEO from '$lib/components/SEO.svelte';
+
+	const seoProps = {
+		title: 'Smile Design by Dr. John Doe',
+		description: 'Smile and Get Back Your Confidence',
+		language: 'en-us',
+		keywords: 'smile design, veneers treatment',
+		type: 'website'
+	};
+</script>
+
+<SEO {...seoProps} />
+
 <h1 class="text-3xl font-bold underline bg-orange-500 text-white">Hello world!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
