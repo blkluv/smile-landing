f0401209b6bcde87d41a166e493748f68af507c4
Gerardo Castillo <gerrcass@gmail.com>
2022-09-26 01:36:40 -0400

    How it works sections + overlay functionality

--- src/lib/components/HowItWorks.svelte
+++ src/lib/components/HowItWorks.svelte
@@ -1,5 +1,127 @@
 <script>
-	//Script
+	import { isOverlayOpen } from '$lib/store/store';
+	import Overlay from '$lib/components/Overlay.svelte';
+
+	import How1Avif from '$lib/assets/howitworks1-263-332.png?width=144;288;432;&format=avif&srcset';
+	import How1Webp from '$lib/assets/howitworks1-263-332.png?width=144;288;432&format=webp&srcset';
+	import How1Png from '$lib/assets/howitworks1-263-332.png?width=432&format=png';
+
+	import How2Avif from '$lib/assets/howitworks2-1620-1620.png?width=176;352;528;&format=avif&srcset';
+	import How2Webp from '$lib/assets/howitworks2-1620-1620.png?width=176;352;528&format=webp&srcset';
+	import How2Png from '$lib/assets/howitworks2-1620-1620.png?width=528&format=png';
+
+	import How3Avif from '$lib/assets/howitworks3-343-259.png?width=144;288;432;&format=avif&srcset';
+	import How3Webp from '$lib/assets/howitworks3-343-259.png?width=144;288;432&format=webp&srcset';
+	import How3Png from '$lib/assets/howitworks3-343-259.png?width=432&format=png';
 </script>
 
-<section>HowItWorks</section>
+<section class="brandGradient bg-[rgb(249,249,249)]">
+	<div class="container mx-auto py-20 space-y-12">
+		<div class="text-center space-y-2 max-w-2xl mx-auto">
+			<p class="text-base font-bold">TREATMENT TIMELINE</p>
+			<h2 class="text-3xl font-extrabold w-[clamp(15ch,15ch,15ch)] mx-auto md:w-auto">
+				YOUR TREATMENT IN 1 WEEK
+			</h2>
+		</div>
+		<div class="flex flex-col space-y-10 max-w-2xl mx-auto items-center md:items-stretch">
+			<div class="flex flex-col-reverse md:flex-row justify-around ml-8 md:ml-0">
+				<div class="mt-8 md:mt-0 min-w-[16rem]">
+					<picture>
+						<source srcset={How1Avif} type="image/avif" />
+						<source srcset={How1Webp} type="image/webp" />
+						<img
+							loading="lazy"
+							decoding="async"
+							src={How1Png}
+							alt="Persona con tratamiento"
+							class="object-cover w-36 mr-auto  md:mx-auto"
+						/>
+					</picture>
+				</div>
+				<div class="flex flex-col self-end min-w-[16rem]">
+					<p
+						class="relative text-3xl font-bold
+							 before:content-['1'] before:block before:absolute before:w-12 before:h-12 before:-left-16 before:text-white before:text-center before:bg-primary before:rounded-full before:pt-2 before:text-3xl before:z-10
+							 md:after:block md:after:absolute md:after:top-8 md:after:-left-10 md:after:border-[1px] md:after:border-primary md:after:h-56 md:after:border-opacity-60 
+							 max-w-[15rem]
+							 "
+					>
+						VIRTUAL CONSULTATION
+					</p>
+					<ul class="list-disc whitespace-nowrap">
+						<li class="ml-6">Consultation with a doctor</li>
+						<li class="ml-6">Take photos</li>
+						<li class="ml-6">Explore financing options</li>
+						<li class="ml-6">Book surgery date</li>
+					</ul>
+				</div>
+			</div>
+			<div class="flex flex-col-reverse md:flex-row-reverse justify-around ml-8 md:ml-0">
+				<div class="mt-8 md:mt-0 min-w-[16rem]">
+					<picture>
+						<source srcset={How2Avif} type="image/avif" />
+						<source srcset={How2Webp} type="image/webp" />
+						<img
+							loading="lazy"
+							decoding="async"
+							src={How2Png}
+							alt="Persona con tratamiento"
+							class="object-cover w-44 mr-auto"
+						/>
+					</picture>
+				</div>
+				<div class="flex flex-col self-end min-w-[16rem]">
+					<p
+						class="relative text-3xl font-bold
+						 		before:content-['2'] before:block before:absolute before:w-12 before:h-12  before:right-[17rem] md:before:-right-20 before:text-white before:text-center before:bg-primary before:rounded-full before:pt-2 before:text-3xl before:z-10
+							 	md:after:block md:after:absolute md:after:top-8 md:after:-right-[3.6rem] md:after:border-[1px] md:after:border-primary md:after:h-48 md:after:border-opacity-60 
+								max-w-[15rem]
+								"
+					>
+						VENEERS CONSULTATION
+					</p>
+					<ul class="list-disc whitespace-nowrap">
+						<li class="ml-6 ">In-depth examination</li>
+						<li class="ml-6 ">X-rays</li>
+						<li class="ml-6 ">Digital Images</li>
+						<li class="ml-6 ">Temporary Veneers</li>
+					</ul>
+				</div>
+			</div>
+			<div class="flex flex-col-reverse md:flex-row justify-around ml-8 md:ml-0">
+				<div class="mt-8 md:mt-0 min-w-[16rem]">
+					<picture>
+						<source srcset={How3Avif} type="image/avif" />
+						<source srcset={How3Webp} type="image/webp" />
+						<img
+							loading="lazy"
+							decoding="async"
+							src={How3Png}
+							alt="Persona con tratamiento"
+							class="object-cover w-36 mr-auto  md:mx-auto"
+						/>
+					</picture>
+				</div>
+				<div class="flex flex-col self-end min-w-[16rem]">
+					<p
+						class="relative text-3xl font-bold
+							 before:content-['3'] before:block before:absolute before:w-12 before:h-12 before:-left-[2.5rem] before:text-white before:text-center before:bg-primary before:rounded-full before:pt-2 before:text-3xl before:z-10
+							 max-w-[18rem]
+							 pl-5"
+					>
+						SMILE MAKEOVER DAY
+					</p>
+					<ul class="list-disc whitespace-nowrap">
+						<li class="ml-6">You’ll have a custom-fit <br /> smile with permanent veneers!</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="flex justify-center">
+			<button class="brandButton" on:click={() => isOverlayOpen.set(true)}>BOOK NOW</button>
+		</div>
+	</div>
+</section>
+{#if $isOverlayOpen}
+	<Overlay />
+{/if}
---
+++ src/lib/components/Overlay.svelte
@@ -0,0 +1,48 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import { isOverlayOpen } from '$lib/store/store';
+	import Form from '$lib/components/Form.svelte';
+
+	import Logo1Avif from '$lib/assets/smylogowhite-3732x3093.png?width=300&format=avif&srcset';
+	import Logo1Webp from '$lib/assets/smylogowhite-3732x3093.png?width=300&format=webp&srcset';
+	import Logo1Png from '$lib/assets/smylogowhite-3732x3093.png?width=192&format=png';
+</script>
+
+<div
+	class="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-gray-800 bg-opacity-90 z-10"
+	on:click={() => isOverlayOpen.set(false)}
+	transition:fade
+>
+	<section
+		on:click|stopPropagation
+		class="max-w-lg px-16  py-10 bg-white  rounded-md relative opacity-100"
+	>
+		<div class="max-w-xs py-5">
+			<button
+				aria-label="Close"
+				class="absolute top-2 right-3 text-4xl text-gray-300 hover:-translate-y-0.5 transition-transform"
+				on:click={() => isOverlayOpen.set(false)}
+			>
+				&times;
+			</button>
+			<header class="flex flex-col items-center  pb-4">
+				<div class="text-center w-24">
+					<picture>
+						<source srcset={Logo1Avif} type="image/avif" />
+						<source srcset={Logo1Webp} type="image/webp" />
+						<img loading="lazy" decoding="async" src={Logo1Png} alt="Persona con tratamiento" />
+					</picture>
+				</div>
+
+				<p
+					class="text-secondary text-3xl font-bold w-[clamp(15ch,15ch,15ch)] text-center leading-10"
+				>
+					We will get back to you!
+				</p>
+			</header>
+			<div class="px-2">
+				<Form />
+			</div>
+		</div>
+	</section>
+</div>
