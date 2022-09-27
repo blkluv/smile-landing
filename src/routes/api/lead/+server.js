96dfeb9f85ad8e99b3d5d470a8c6436bc94a69e9
Gerardo Castillo <gerrcass@gmail.com>
2022-09-26 23:05:46 -0400

    formating error back to console20

--- src/routes/api/lead/+server.js
+++ src/routes/api/lead/+server.js
@@ -1,9 +1,9 @@
-import { error } from '@sveltejs/kit';
+/* import { error } from '@sveltejs/kit';
 import cookie from 'cookie';
 import { getEdgeUserData } from '$lib/utils/cfEdgeProperties'
 import { PRODUCT_NAME, PRODUCT_CATEGORY } from '$lib/store/store'
 import { sha256 } from '$lib/utils/crypto'
-import { sendEventToCapi } from '$lib/utils/sendEventToCapi'
+import { sendEventToCapi } from '$lib/utils/sendEventToCapi' */
 
 /** @type {import('./$types').RequestHandler} */
 
