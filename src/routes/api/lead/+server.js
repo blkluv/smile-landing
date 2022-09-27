1a44807694e0cdc67b5c15f213849dea411c8b40
Gerardo Castillo <gerrcass@gmail.com>
2022-09-26 22:28:51 -0400

    formating error back to console14

--- src/routes/api/lead/+server.js
+++ src/routes/api/lead/+server.js
@@ -8,7 +8,7 @@ import { sendEventToCapi } from '$lib/utils/sendEventToCapi'
 /** @type {import('./$types').RequestHandler} */
 export const POST = async ({ request, url, clientAddress, platform }) => {
     platform.env.LOGS && await platform.env.LOGS.put('FLAG' + (+new Date()), 'FLAG')
-    return json(200, 'NICE')
+    return json('NICE')
     try {
         const { firstname, lastname, email, phone, leadEventID } = await request.json()
 
